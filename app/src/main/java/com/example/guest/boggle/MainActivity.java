0d0c1488b2568f1a0f376803e445a8b32c7b3b12
Jeffrey Hackford and Noah Kittleson <noah@>
2016-06-28 11:35:51 -0700

    display boggle letters and accept user input

--- app/src/main/java/com/example/guest/boggle/MainActivity.java
+++ app/src/main/java/com/example/guest/boggle/MainActivity.java
@@ -3,6 +3,9 @@ package com.example.guest.boggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.util.ArrayList;
@@ -13,12 +16,43 @@ public class MainActivity extends AppCompatActivity {
     private static final Character[] vowels = new Character[] {'a', 'e', 'i', 'o', 'u', 'y'};
     private ArrayList<Character> boggleArray = new ArrayList<Character>();
     private TextView boggleLetters;
+    private Button submitWordButton;
+    private TextView wordList;
+    private EditText wordInput;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         boggleLetters = (TextView) findViewById(R.id.boggleLetters);
+        wordList = (TextView) findViewById(R.id.wordList);
+        wordInput = (EditText) findViewById(R.id.wordInput);
+        submitWordButton = (Button) findViewById(R.id.submitWord);
+        submitWordButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String[] userWord = wordInput.getText().toString().split("");
+                for(int i = 0; i <= userWord.length; i++) {
+                    for(int j = 0; j < 8; j++) {
+                        if (Character.toString(boggleArray.get(j)) == userWord[i]) {
+                            //remove j
+                            Log.d(Character.toString(boggleArray.get(j)), "Character match!");
+                            j = -1;
+                            i++;
+                            if (i > userWord.length) {
+                                wordList.append(" " + wordInput.getText().toString());
+                                Log.d("GOOD WORD", "GOOD WORD");
+                                return;
+                            }
+                        }
+                    }
+                    //that's bad
+                    Log.d("BAD WORD", "BAD WORD");
+                    return;
+                }
+                wordList.append(" " + wordInput.getText().toString());
+            }
+        });
         Random rng = new Random();
         for (int i = 0; i < 6; i++) {
             boggleArray.add(consonants[rng.nextInt(consonants.length)]);
@@ -33,6 +67,8 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 8; i++) {
             boggleLetters.append(Character.toString(boggleArray.get(i)));
         }
+
+
 
     }
 }
