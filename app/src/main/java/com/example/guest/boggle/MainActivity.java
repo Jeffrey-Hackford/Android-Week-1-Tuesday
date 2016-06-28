854537d667dace3d32b3972737f4e60002dc5ff7
Jeffrey Hackford and Noah Kittleson <noah@>
2016-06-28 14:34:27 -0700

    add comparison checking to validate user input and implement butterknife

--- app/src/main/java/com/example/guest/boggle/MainActivity.java
+++ app/src/main/java/com/example/guest/boggle/MainActivity.java
@@ -7,6 +7,8 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -15,42 +17,38 @@ public class MainActivity extends AppCompatActivity {
     private static final Character[] consonants = new Character[]  { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
     private static final Character[] vowels = new Character[] {'a', 'e', 'i', 'o', 'u', 'y'};
     private ArrayList<Character> boggleArray = new ArrayList<Character>();
-    private TextView boggleLetters;
-    private Button submitWordButton;
-    private TextView wordList;
-    private EditText wordInput;
+    @Bind(R.id.wordInput) EditText wordInput;
+    @Bind(R.id.wordList) TextView wordList;
+    @Bind(R.id.submitWord) Button submitWordButton;
+    @Bind(R.id.boggleLetters) TextView boggleLetters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        boggleLetters = (TextView) findViewById(R.id.boggleLetters);
-        wordList = (TextView) findViewById(R.id.wordList);
-        wordInput = (EditText) findViewById(R.id.wordInput);
-        submitWordButton = (Button) findViewById(R.id.submitWord);
+        ButterKnife.bind(this);
+
         submitWordButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String[] userWord = wordInput.getText().toString().split("");
-                for(int i = 0; i <= userWord.length; i++) {
-                    for(int j = 0; j < 8; j++) {
-                        if (Character.toString(boggleArray.get(j)) == userWord[i]) {
-                            //remove j
-                            Log.d(Character.toString(boggleArray.get(j)), "Character match!");
-                            j = -1;
-                            i++;
-                            if (i > userWord.length) {
-                                wordList.append(" " + wordInput.getText().toString());
-                                Log.d("GOOD WORD", "GOOD WORD");
-                                return;
+                ArrayList<Character> boggleCopy = new ArrayList<Character> (boggleArray);
+                if(userWord.length > 3) {
+                    for (int i = 1; i < userWord.length; i++) {
+                        for (int j = 0; j < boggleCopy.size(); j++) {
+                            if (userWord[i].equals(Character.toString(boggleCopy.get(j)))) {
+                                boggleCopy.remove(j);
+                                j = -1;
+                                i++;
+                                if (i > userWord.length - 1) {
+                                    wordList.append(" " + wordInput.getText().toString());
+                                    return;
+                                }
                             }
                         }
+                        return;
                     }
-                    //that's bad
-                    Log.d("BAD WORD", "BAD WORD");
-                    return;
                 }
-                wordList.append(" " + wordInput.getText().toString());
             }
         });
         Random rng = new Random();
@@ -67,8 +65,5 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < 8; i++) {
             boggleLetters.append(Character.toString(boggleArray.get(i)));
         }
-
-
-
     }
 }
