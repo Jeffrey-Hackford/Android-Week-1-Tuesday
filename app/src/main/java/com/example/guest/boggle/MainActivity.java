bcb36b59f1fc3dceae78b3a5ff08fd260eda8335
Jeffrey Hackford and Noah Kittleson <jeffrey@>
2016-06-28 10:18:54 -0700

    we get the characters to display on screen

--- app/src/main/java/com/example/guest/boggle/MainActivity.java
+++ app/src/main/java/com/example/guest/boggle/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.guest.boggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
 
 import java.util.ArrayList;
 import java.util.Random;
@@ -11,11 +12,13 @@ public class MainActivity extends AppCompatActivity {
     private static final Character[] consonants = new Character[]  { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
     private static final Character[] vowels = new Character[] {'a', 'e', 'i', 'o', 'u', 'y'};
     private ArrayList<Character> boggleArray = new ArrayList<Character>();
+    private TextView boggleLetters;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        boggleLetters = (TextView) findViewById(R.id.boggleLetters);
         Random rng = new Random();
         for (int i = 0; i < 6; i++) {
             boggleArray.add(consonants[rng.nextInt(consonants.length)]);
@@ -26,5 +29,10 @@ public class MainActivity extends AppCompatActivity {
         for (int i = 0; i < boggleArray.size(); i++) {
             Log.d(Character.toString(boggleArray.get(i)), "BoggleString");
         }
+
+        for (int i = 0; i < 8; i++) {
+            boggleLetters.append(Character.toString(boggleArray.get(i)));
+        }
+
     }
 }
