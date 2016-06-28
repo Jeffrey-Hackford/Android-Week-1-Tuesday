da7274e0f8a2613a926e50da0d02d3e8b594e41b
Jeffrey Hackford and Noah Kittleson <jeffrey@>
2016-06-28 09:46:06 -0700

    add loops to populate a boggle Array with 8 letters

---
+++ app/src/main/java/com/example/guest/boggle/MainActivity.java
@@ -0,0 +1,30 @@
+package com.example.guest.boggle;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+    private static final Character[] consonants = new Character[]  { 'b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z' };
+    private static final Character[] vowels = new Character[] {'a', 'e', 'i', 'o', 'u', 'y'};
+    private ArrayList<Character> boggleArray = new ArrayList<Character>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Random rng = new Random();
+        for (int i = 0; i < 6; i++) {
+            boggleArray.add(consonants[rng.nextInt(consonants.length)]);
+        }
+        for (int i = 0; i < 2; i++) {
+            boggleArray.add(vowels[rng.nextInt(vowels.length)]);
+        }
+        for (int i = 0; i < boggleArray.size(); i++) {
+            Log.d(Character.toString(boggleArray.get(i)), "BoggleString");
+        }
+    }
+}
